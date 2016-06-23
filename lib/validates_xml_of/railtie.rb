9710ea9e8380564f0709c462384c865dbdc306bb
Bruno Arueira <contato@brunoarueira.com>
2016-06-23 12:57:02 -0300

    Unused block argument

--- lib/validates_xml_of/railtie.rb
+++ lib/validates_xml_of/railtie.rb
@@ -1,6 +1,6 @@
 module ValidatesXmlOf
   class Railtie < Rails::Railtie
-    initializer 'validates_xml_of.load_i18n_locales' do |app|
+    initializer 'validates_xml_of.load_i18n_locales' do
       ValidatesXmlOf::load_i18n_locales
     end
   end
